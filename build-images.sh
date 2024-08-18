1dc1d32cf6c132d23d43bba825221dfed315af23
kemboi22 <kemboielvis22@gmail.com>
2024-08-19 01:42:31 +0300

    feat: added necessary images

--- build-images.sh
+++ build-images.sh
@@ -11,10 +11,10 @@ set -e
 # Prepare variables for later use
 images=()
 # The image will be pushed to GitHub container registry
-repobase="${REPOBASE:-ghcr.io/nethserver}"
+repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="leantime"
-
+leantime_version="3.1.4"
 # Create a new empty container image
 container=$(buildah from scratch)
 
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/nginx:stable-alpine3.17" \
+    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/leantime/leantime:${leantime_version}" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
